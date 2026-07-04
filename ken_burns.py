d0ab33687f6930a3754cb45b0c3ce3d97d66b3d6
Nikki Cooper <nikki.lynn.cooper@gmail.com>
2026-07-04 18:32:49 -0500

    Added copyright and info header to top of file

--- ken_burns.py
+++ ken_burns.py
@@ -1,4 +1,10 @@
 #!/usr/bin/env python3
+#  ken_burns.py Copyright (c) 2026 Nikki Cooper
+#
+#  This program and the accompanying materials are made available under the
+#  terms of the GNU Lesser General Public License, version 3.0 which is available at
+#  https://www.gnu.org/licenses/gpl-3.0.html#license-text
+#
 import cv2
 import numpy as np
 import os
